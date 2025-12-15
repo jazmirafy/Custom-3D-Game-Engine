04a9eca51905a703bb8c74a225737a7a3ff75dc7
jazmirafy <117775854+jazmirafy@users.noreply.github.com>
2025-12-15 14:51:56 -0500

    created shader program and rendered a triangle to my window!

--- source/main.cpp
+++ source/main.cpp
@@ -1,5 +1,8 @@
+#include <GL/glew.h>
 #include <GLFW/glfw3.h>
 #include <iostream>
+#include <vector>
+#include <string>
 int main()
 {
     //initialize library
@@ -8,7 +11,13 @@ int main()
     if (!glfwInit()) {
         return -1;
     }
-    //otherwise, attempt to create a window
+
+    //let glfw know which open gl context is intended to use
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    //attempt to create a window
     GLFWwindow* window = glfwCreateWindow(1200, 720, "Egypt's Game Engine", nullptr, nullptr);
 
 
@@ -25,7 +34,137 @@ int main()
     //set window position
     //glfwSetWindowPos(window, -, -);
 
+    //specifying which window our open gl context needs to be associated with
+    glfwMakeContextCurrent(window);
+
+    //if we fail to initialize glew library terminate the program
+    if (glewInit() != GLEW_OK) {
+        glfwTerminate();
+        return -1;
+    }
+    //vertex shader source code
+    std::string vertexShaderSource = R"(
+        #version 330 core
+        layout (location = 0) in vec3 position;
+
+        void main(){
+            gl_Position = vec4(position.x, position.y, position.z, 1.0);
+        }
+    )";
+    //create shader in graphics card
+    GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
+
+    //compile vertex shader
+    const char* vertexShaderCstr = vertexShaderSource.c_str();
+    glShaderSource(vertexShader, 1, &vertexShaderCstr, nullptr);
+    glCompileShader(vertexShader);
+
+    //check if vertex shader compilation was successful
+    GLint success;
+    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+    
+    //if vertex shader compilation is unsuccessful, display the error
+    if (!success) {
+
+        char infoLog[512];
+        glGetShaderInfoLog(vertexShader, 512, nullptr, infoLog);
+        std::cerr << "ERROR: VERTEX_SHADER_COMPILATION_FAILED: " << infoLog << std::endl;
+    }
+
+    //fragment shader source code
+    std::string fragmentShaderSource = R"(
+        #version 330 core
+        out vec4 FragColor;
+        
+        void main(){
+            FragColor = vec4(1.0, 0.0, 0.0, 1.0);
+        }
+    )";
+    //compile fragment shader
+    GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    const char* fragmentShaderCstr = fragmentShaderSource.c_str();
+    glShaderSource(fragmentShader, 1, &fragmentShaderCstr, nullptr);
+    glCompileShader(fragmentShader);
+
+    //check if fragment shader compilation was successful
+    glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+
+    //if fragment shader compilation is unsuccessful, display the error
+    if (!success) {
+        
+        char infoLog[512];
+        glGetShaderInfoLog(fragmentShader, 512, nullptr, infoLog);
+        std::cerr << "ERROR:FRAGMENT_SHADER_COMPILATION_FAILED: " << infoLog << std::endl;
+    }
+
+    //combine vertex and fragment shaders into a single shader program
+    //create obeject for shader program in the graphics card
+    GLuint shaderProgram = glCreateProgram();
+    //attach all shaders to this program
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    //link shaders together in the graphics pipeline
+    glLinkProgram(shaderProgram);
+
+    //check link status, check for errors
+    glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+
+    //if there is an error when linking, display the error
+    if (!success) {
+        char infoLog[512];
+        glGetProgramInfoLog(shaderProgram, 512, nullptr, infoLog);
+        std::cerr << "ERROR:SHADER_PROGRAM_LINKING_FAILED: " << infoLog << std::endl;
+
+    }
+
+    //once the shader program has successfully linked we no longer need the individual shader objects
+    // delete individual shader objects from gpu memory
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+
+    
+
+    //creating triangle structure
+    std::vector<float> vertices =
+    {
+        0.0f, 0.5f, 0.0f,
+        -0.5f, -0.5f, 0.0f,
+        0.5f, -0.5f, 0.0f
+    };
+
+    //upload triangle vertex data to gpu memory using buffer
+    GLuint vbo;
+    glGenBuffers(1, &vbo);
+    glBindBuffer(GL_ARRAY_BUFFER, vbo);
+    glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(float), vertices.data(), GL_STATIC_DRAW);
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+    //tell graphics pipeline/shader program how to interpret our data when it reaches the vertex shader
+    GLuint vao;
+    glGenVertexArrays(1, &vao);
+    glBindVertexArray(vao);
+    glBindBuffer(GL_ARRAY_BUFFER, vbo);
+    glVertexAttribPointer(0, 3, GL_FLOAT, false, 3 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindVertexArray(0);
+
     while(!glfwWindowShouldClose(window)) {
+        
+        //change window background color
+        glClearColor(0.0f, 0.0f, 1.0f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        //activate shader program
+        glUseProgram(shaderProgram);
+        //bind vao containing our vertex layout
+        glBindVertexArray(vao);
+        //render triangle
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+        //swapping the buffer so we can actually see the colored background
+        glfwSwapBuffers(window);
+
+
         glfwPollEvents();
     }
 
