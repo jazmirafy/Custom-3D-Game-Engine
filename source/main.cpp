ff580396e8201e52b98493ed513deb9fb9c34486
jazmirafy <117775854+jazmirafy@users.noreply.github.com>
2025-12-15 10:51:19 -0500

    Created game engine window yayyyy
    My game engine now has a window!

--- source/main.cpp
+++ source/main.cpp
@@ -1,4 +1,36 @@
+#include <GLFW/glfw3.h>
+#include <iostream>
 int main()
 {
+    //initialize library
+    
+    //if we failed to initialize the glfw library stop the program
+    if (!glfwInit()) {
+        return -1;
+    }
+    //otherwise, attempt to create a window
+    GLFWwindow* window = glfwCreateWindow(1200, 720, "Egypt's Game Engine", nullptr, nullptr);
+
+
+    //check whether the window was successfully created
+    
+    // if failed notify the user, terminate the glfw library and exit the program
+    if (window == nullptr) {
+        std::cout << "Error creating window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    //if the window was successfully created enter the main event loop
+
+    //set window position
+    //glfwSetWindowPos(window, -, -);
+
+    while(!glfwWindowShouldClose(window)) {
+        glfwPollEvents();
+    }
+
+    //if we have exited the while loop above the window has been requested to be closed so now clean and clear the glfw library
+    glfwTerminate();
     return 0;
+
 }
