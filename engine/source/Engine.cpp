887d46545c1ac58d51a4c8d52a7c91b164d6a8d3
jazmirafy <117775854+jazmirafy@users.noreply.github.com>
2026-01-07 23:59:41 -0500

    adding engine and application base classes
    created basic structure and life cycle of the engine

---
+++ engine/source/Engine.cpp
@@ -0,0 +1,49 @@
+#include "Engine.h"
+#include "Application.h"
+
+namespace eng {
+
+	bool Engine::Init() {
+		
+		if (!m_application) {
+			return false;
+		}
+		return m_application->Init();
+	}
+
+	
+	void Engine::Run() {
+		//check for valid application
+		if (!m_application) {
+			return;
+		}
+
+		m_lastTimePoint = std::chrono::high_resolution_clock::now();
+		//main game loop lives here
+		while (!m_application->NeedsToBeClosed()) {
+
+			//each frame compute delta time from the current time
+			auto now = std::chrono::high_resolution_clock::now();
+			float deltaTime = std::chrono::duration<float>(now - m_lastTimePoint).count();
+			m_lastTimePoint = now;
+
+			m_application->Update(deltaTime);
+
+
+		}
+	}
+	void Engine::Destroy() {
+
+		if (m_application) {
+			m_application->Destroy();
+			m_application.reset();
+		}
+	}
+	void Engine::SetApplication(Application * app) {
+		m_application.reset(app);
+	}
+	Application* Engine::GetApplication() {
+		return m_application.get();
+	}
+	
+}
